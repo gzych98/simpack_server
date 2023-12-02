27458685d7a9dcb4919424fe1403ab95704c01e4
Grzegorz Zych <111496194+gzych98@users.noreply.github.com>
2023-12-03 00:23:29 +0100

    działa lista plików, działa przesuwanie ręczne

--- app.py
+++ app.py
@@ -23,30 +23,42 @@ def home():
 
 @app.route('/upload_files', methods=['GET', 'POST'])
 def upload_file():
+    messages = []
     if request.method == 'POST':
         files = request.files.getlist('file')
-        for file in files:
-            if file and allowed_file(file.filename):
-                filepath = os.path.join(app.config['UPLOAD_FOLDER'], file.filename) 
-                file.save(filepath)
-                #rozpakuj pliki
-                with zipfile.ZipFile(filepath) as zip_ref:
-                    zip_ref.extractall(app.config['UPLOAD_FOLDER'])
-        return 'Plik został przesłany'
-    return render_template('upload.html')
+        if not files:
+            messages.append('Nie wybrano żadnych plików.')
+        else:
+            for file in files:
+                if file and allowed_file(file.filename):
+                    filepath = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
+                    file.save(filepath)
+                    # Rozpakuj pliki
+                    with zipfile.ZipFile(filepath) as zip_ref:
+                        zip_ref.extractall(app.config['UPLOAD_FOLDER'])
+                    messages.append(f"File uploaded: {file.filename}")
+                else:
+                    messages.append(f"Extension error: {file.filename}. Only .zip files accepted.<br>")
+    return render_template('upload.html', messages=messages)
+
 
 @app.route('/filelist')
 def filelist():
     files = [f for f in os.listdir(app.config['UPLOAD_FOLDER']) if f.endswith(app.config['EXTENSION_LIST'])] 
     return render_template('filelist.html', files=files)
 
-@app.route ('/update_order', methods=['POST'])
+@app.route('/update_order', methods=['GET', 'POST'])
 def update_order():
     global current_order
     new_order = request.json
     current_order = new_order
     print(f"-- Current file order: {' | '.join(map(str, current_order))}")
     return jsonify({"success": True, "new_order": current_order})
+
+@app.route('/get_current_order', methods=['GET'])
+def get_current_order():
+    global current_order
+    return jsonify({"current_order": current_order})
 
 if __name__ == '__main__':
     app.run(debug=True)
